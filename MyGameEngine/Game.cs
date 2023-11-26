1c7eed413bdad7e7f9d797321e41caf68630d627
Vlad <115177420+MelnikVladyslav@users.noreply.github.com>
2023-11-26 22:21:26 +0200

    Add Shaders

--- MyGameEngine/Game.cs
+++ MyGameEngine/Game.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK.Graphics.OpenGL4;
+﻿using MyGameEngine.TestItems;
+using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -7,8 +8,79 @@ namespace MyGameEngine
 {
     public class Game : GameWindow
     {
+        int VertexBufferObject;
+        Triangle testTr = new Triangle();
+        Shader shader;
+        private int VertexArrayObject;
+
         public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
 
+        //Start(Initilization)
+        protected override void OnLoad()
+        {
+            base.OnLoad();
 
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            // Генерація та компіляція вершинного та фрагментного шейдерів
+            shader = new Shader("shader.vert", "shader.frag");
+
+            // Генерація та зв'язування Vertex Buffer Object (VBO)
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, testTr.vertices.Length * sizeof(float), testTr.vertices, BufferUsageHint.StaticDraw);
+
+            // Встановлення вказівників атрибутів вершин
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            // Генерація та зв'язування Vertex Array Object (VAO)
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+        }
+
+        //Renderer
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            shader.Use();
+
+            GL.UseProgram(shader.Handle);
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            SwapBuffers();
+        }
+
+        //Update size window
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+
+        //Update frame
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            //На Escape -> Exit
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        //Unload
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            shader.Dispose();
+        }
     }
 }
---
+++ MyGameEngine/TestItems/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameEngine.TestItems
+{
+    public class Triangle
+    {
+        public float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+             0.5f, -0.5f, 0.0f, //Bottom-right vertex
+             0.0f,  0.5f, 0.0f  //Top vertex
+        };
+    }
+}
